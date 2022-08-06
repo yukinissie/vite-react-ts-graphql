8b085b14d47f119171c698fbd320a607565faa27
yukinissie <bigtree2490@outlook.jp>
2022-08-06 13:26:41 +0900

    query graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      id
+      name
+      email
+    }
+  }
+`
+
 function App() {
-  return <h1>GraphQL</h1>
+  const { data, loading, error } = useQuery(GET_USERS)
+
+  if (loading) return <p>ローディング中です</p>
+  if (error) return <p>エラーが発生しています。</p>
+
+  return (
+    <div style={{ margin: '3em' }}>
+      <h1>GraphQL</h1>
+      {data.users.map((user: any) => (
+        <div key={user.id}>Name: {user.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default App
